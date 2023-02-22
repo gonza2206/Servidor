312f09fe8b25ff30bdda0ad3563373ee6f24da28
gonza3008 <gonzalo.lopez@sitrack.com>
2023-02-22 09:10:04 -0300

    Update Schema.js

--- schema/data-schema.js
+++ schema/data-schema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 //const { default: dataModel } = require('../schema/data-schema');//importo el esquema
 const meassureSchema = mongoose.Schema({
     meassure: String,
-    date: Date
+    date: Date,
 });
 
 const dataModel = mongoose.model('Meassure', meassureSchema);
